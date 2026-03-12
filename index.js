a68ac1579dcb96302b179dd2081e34770b61053b
Jaloliddin Gulimov <gulimovjalol1999-bit>
2026-03-12 22:56:41 +0500

    exam

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 const express = require("express")
 const cors = require("cors")
 const connectDb = require("./config/db.config")
-const authorRouter = require("./router/author.routes")
-const bookRouter = require("./router/book.routes")
 const errorMiddleware = require("./middleware/error-middleware")
 const authRouter = require("./router/auth.routes")
 const cookieParser = require("cookie-parser")
-const quoteRouter = require("./router/quote.routes")
-const profileRouter = require("./router/profile.routes")
 const logger = require("./utils/logger")
 const carRouter = require("./router/car.routes")
 const modelRouter = require("./router/model.routes")
+const profileRouter = require("./router/profile.routes")
 require("dotenv").config()
 
 const PORT = process.env.PORT || 3000 
@@ -23,10 +20,7 @@ app.use(cookieParser())
 
 
 //router
-app.use(authorRouter)
-app.use(bookRouter) 
 app.use(authRouter)
-app.use(quoteRouter)
 app.use(profileRouter)
 app.use(modelRouter)
 app.use(carRouter)
